e9d00185bc602eaefdffb1efd44860cd5a63ce28
nicolasamoroso <108936163+nicolasamoroso@users.noreply.github.com>
2026-02-24 19:34:06 -0300

    new web design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "media",
   theme: {
-    screens: {
-      xs: "475px",
-      sm: "540px",
-      md: "668px",
-      lg: "820px",
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "-apple-system", "BlinkMacSystemFont", "sans-serif"],
+      },
+      maxWidth: {
+        "2xl": "42rem",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
